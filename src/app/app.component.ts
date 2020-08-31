fb7601359c9b23ec8eca4fe15455b9ca96ad1c43
narendraApptrinity <narendra@apptrinity.com>
2020-08-31 17:03:49 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { SpacexService } from './services/spacex.service'
+import { SpacexService } from './services/spacex.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  //   title = 'SpaceXproject';
-  selectedItem: any = "";
-  launchValue: any = "";
-  landingValue: any = "";
+  selectedItem: any = '';
+  launchValue: any = '';
+  landingValue: any = '';
   queryObject: any = {};
 
-  //   yearArray: any[] = [2006, 2007, 2008, 2009];
-  booleanArray: any[] = ["true", "false"];
+  booleanArray: any[] = ['true', 'false'];
 
   tableData: any = [];
   launchYear: any = [];
@@ -22,31 +20,20 @@ export class AppComponent implements OnInit {
 
   constructor(private service: SpacexService) { }
 
-  ngOnInit() {
-    this.service.fetchData().subscribe(data => {
-      this.tableData = data;
-      console.log(data)
-    })
 
-    for (let i = 2006; i <= 2020; i++) {
-      this.launchYear.push(i);
-    }
-  }
 
   queryHandler() {
-    console.log("queryObject=====", this.queryObject);
     this.service.fetchQueryData(this.queryObject).subscribe(data => {
       this.tableData = data;
-      console.log(data)
-    })
+    });
   }
 
+  // tslint:disable-next-line: typedef
   yearClick(event, value) {
-    // alert("hi")
-    console.log(event, value);
-    if (this.selectedItem == value) {
-      this.selectedItem = "";
-      delete this.queryObject["launch_year"];
+
+    if (this.selectedItem === value) {
+      this.selectedItem = '';
+      delete this.queryObject.launch_year;
     }
     else {
       this.selectedItem = value;
@@ -55,10 +42,9 @@ export class AppComponent implements OnInit {
     this.queryHandler();
   }
   launchClick(event, value) {
-    console.log(event, value);
-    if (this.launchValue == value) {
-      this.launchValue = "";
-      delete this.queryObject["launch_success"];
+    if (this.launchValue === value) {
+      this.launchValue = '';
+      delete this.queryObject.launch_success;
 
     }
     else {
@@ -71,23 +57,22 @@ export class AppComponent implements OnInit {
   }
 
   landingClick(event, value) {
-    console.log(event, value);
-    if (this.landingValue == value) {
-      this.landingValue = "";
-      delete this.queryObject["land_success"];
-
+    if (this.landingValue === value) {
+      this.landingValue = '';
+      delete this.queryObject.land_success;
     }
     else {
-      this.landingValue = value
+      this.landingValue = value;
       this.queryObject.land_success = value;
-
     }
     this.queryHandler();
-
+  }
+  ngOnInit() {
+    this.service.fetchData().subscribe(data => {
+      this.tableData = data;
+    });
+    for (let i = 2006; i <= 2020; i++) {
+      this.launchYear.push(i);
+    }
   }
 }
-
-   // var target = event.target || event.srcElement || event.currentTarget;
-    // console.log(target.attributes.id, target.attributes.class);
-    // let classes = target.attributes.class;
-    // classes.removeClass('btns-selected')
